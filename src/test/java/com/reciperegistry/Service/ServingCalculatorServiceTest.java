69179cd08721dfad1786f99c97a3d03f55979f23
Szatmári Mihály <mihaly.szatmari@sightspot.hu>
2018-05-14 00:27:11 +0200

    Added unit test to the project.

--- src/test/java/com/reciperegistry/Service/ServingCalculatorServiceTest.java
+++ src/test/java/com/reciperegistry/Service/ServingCalculatorServiceTest.java
@@ -1,18 +1,39 @@
 package com.reciperegistry.Service;
 
+import com.reciperegistry.Entity.Ingredient;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServingCalculatorServiceTest {
+    private ServingCalculatorService servingCalculatorService;
+
+    private List<Ingredient> ingredientList;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        this.servingCalculatorService = new ServingCalculatorService();
+
+        Ingredient ingredient;
+        ingredient = new Ingredient("TestIngredient", 10.00, 5, "", 50.00, 1);
+
+        this.ingredientList = new ArrayList<>();
+        this.ingredientList.add(ingredient);
     }
 
     @Test
     public void getCalculatedRecipe() {
-        assertEquals(5, 2 + 3);
+        Double result = this.servingCalculatorService
+                .getCalculatedIngredients(this.ingredientList, 2, 6)
+                .get(0).getQuantity();
+
+        assertEquals((Double) 30.00, result);
     }
 }
